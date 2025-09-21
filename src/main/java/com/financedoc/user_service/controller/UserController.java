8a054c59598a2b0b56aad4afdce8f136cfee0337
rudalsss <linda284@naver.com>
2025-09-21 11:31:52 +0900

    init: user-service spring starter project

---
+++ src/main/java/com/financedoc/user_service/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.financedoc.user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+    @GetMapping("/test")
+    public String test() {
+        return "User Service Test OK";
+    }
+}
