ada61461b1004da6124862ab2c00aae9c0c6d7d4
rudalsss <linda284@naver.com>
2025-09-23 12:00:49 +0900

    fix: X-User-Email 헤더를 선택적 처리하여 400 에러 방지

--- src/main/java/com/financedoc/user_service/controller/UserController.java
+++ src/main/java/com/financedoc/user_service/controller/UserController.java
@@ -8,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @RestController
 public class UserController {
+
     @GetMapping("/test")
-    public String test(@RequestHeader("X-User-Email") String email) {
-        return "User Service Test OK & X-User-Email = "+ email;
+    public String test(
+            @RequestHeader(value = "X-User-Email", required = false) String email
+    ){
+        if (email == null) {
+            email = "null token"; // 기본값
+        }
+
+        return "User Service Test OK & X-User-Email = "+email;
     }
+
 }
