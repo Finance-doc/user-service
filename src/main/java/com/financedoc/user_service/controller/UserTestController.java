d9f04b6864ba7ddf86159fb3369b48ee8814d747
rudalsss <linda284@naver.com>
2025-09-29 00:18:24 +0900

    fix: UserTestController오류 수정

---
+++ src/main/java/com/financedoc/user_service/controller/UserTestController.java
@@ -0,0 +1,23 @@
+package com.financedoc.user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserTestController {
+
+    @GetMapping("/test")
+    public String test(
+            @RequestHeader(value = "X-User-Email", required = false) String email
+    ){
+        if (email == null) {
+            email = "null token"; // 기본값
+        }
+
+        return "User Service Test OK & X-User-Email = "+email;
+    }
+
+}
